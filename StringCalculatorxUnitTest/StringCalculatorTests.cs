88efda26236b202a4de05b612515ca19cd16396d
Amit Kumar <akumar1@lamar.edu>
2018-09-12 15:27:43 -0400

    Test failed needd to change the class method add

--- StringCalculatorxUnitTest/StringCalculatorTests.cs
+++ StringCalculatorxUnitTest/StringCalculatorTests.cs
@@ -45,7 +45,7 @@ namespace StringCalculatorxUnitTest
         [Fact]
         public void ToReturnSumOfStringsWhenCustomDelimiters()
         {
-            Assert.Throws<ArgumentException>(() => new Calculator().Add(",\n"));
+            "//[***]\n1***2***3".StringAddTpNumbers(6);
         }
     }
 
