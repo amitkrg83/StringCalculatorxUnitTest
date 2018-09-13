5de6dc88795d2ad41100ce0852daf5fe8bbde7ff
Amit Kumar <akumar1@lamar.edu>
2018-09-13 11:42:22 -0400

    Interface added for eventHandler

---
+++ Calculator.CQRS/EventHandlers/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calculator.CQRS.Events;
+
+namespace Calculator.CQRS.EventHandlers
+{
+    public interface IEventHandler<TEvent> where TEvent : Event
+    {
+        void Handle(TEvent handle);
+    }
+}
